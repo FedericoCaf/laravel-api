3f13042847a5325f979465b8d08dc5915b64899a
FedericoCaf <fedetheshield09@gmail.com>
2022-02-09 18:53:20 +0100

    vue-ok

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <h1>HOME VUE</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-})->name('home');
+
 
 Auth::routes();
 
@@ -28,3 +26,7 @@ Route::middleware('auth')
 
        Route::resource('/posts', 'PostController');
   });
+
+  Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any' , '.*')->name('home');
